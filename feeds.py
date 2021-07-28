03598aa26a64fedb35c75c6c07ff363bed28213a
filinpavel <atmega644@gmail.com>
2021-07-28 20:47:27 +0700

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -5,5 +5,9 @@ st.markdown('### NKCKI feed')
 r = 5
 for n in range(r) :
   datesplit = (nkcki.entries[n].published).split()
-  a = datesplit[1]+' '+datesplit[2] +' '+ nkcki.entries[n].title +' '+ nkcki.entries[n].link
-  st.write(a)
+  date = datesplit[1]+' '+datesplit[2]
+  #articlelink =' '+ nkcki.entries[n].title +' '+ nkcki.entries[n].link
+  #articlelink =' '+nkcki.entries[n].title+' '+nkcki.entries[n].link
+  #a = data + articlelink
+  #st.write(a)
+  st.markdown('date')
