17957ed95ba79ae2f91b2247bc19ba3ea43fec70
filinpavel <atmega644@gmail.com>
2021-07-28 23:00:38 +0700

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -7,7 +7,7 @@ dict = {'НКЦКИ':'https://safe-surf.ru/rss/',
         'NSA':'https://www.nsa.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=920&max=20',
         'Securelist':'https://securelist.com/feed',
         'ESET':'https://www.welivesecurity.com/feed/'}
-r = st.slider('more News ?',1,6,1)
+r = st.slider('',1,5,1)
 d = len(dict)
 for i in range(d) :
     for n in range(r):
