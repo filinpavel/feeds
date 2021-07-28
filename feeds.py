217880876062e5259f329a482dcd60d40fb7d0af
filinpavel <atmega644@gmail.com>
2021-07-28 17:57:50 +0700

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.markdown('_Markdown_')
+import feedparser
+nkcki = feedparser.parse('https://safe-surf.ru/rss/')
+st.markdown('### NKCKI feed')
+r = 5
+for n in range(r) :
+  datesplit = (nkcki.entries[n].published).split()
+  a = datesplit[1]+' '+datesplit[2] +' '+ nkcki.entries[n].title +' '+ nkcki.entries[n].link
+  st.write(a)
