84b5a4373277a242a36a3f8f31dc81afa6194185
filinpavel <atmega644@gmail.com>
2021-07-28 23:15:26 +0700

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -7,7 +7,8 @@ dict = {'НКЦКИ':'https://safe-surf.ru/rss/',
         'NSA':'https://www.nsa.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=920&max=20',
         'Securelist':'https://securelist.com/feed',
         'ESET':'https://www.welivesecurity.com/feed/'}
-r = st.slider('',1,5,1)
+#r = st.slider('',1,5,1)
+r = 3
 d = len(dict)
 for i in range(d) :
     for n in range(r):
@@ -15,8 +16,10 @@ for i in range(d) :
       url = list(dict.values())[i]
       article = feedparser.parse(url)
       datesplit = (article.entries[n].published).split()
-      date = datesplit[1]+' '+datesplit[2]
-      articlelink =' '+article.entries[n].title+' '+article.entries[n].link
-      a = id +' '+ date +' '+ articlelink
+      date = datesplit[1] + ' ' + datesplit[2]
+      articlelink = ' ' + article.entries[n].title + ' ' + article.entries[n].link
+      #a = id + ' ' + date + ' ' + articlelink
+      a = id+' '+ date + ' ' + article.entries[n].title
       st.write(a)
-  
+      a = '\t' + '\t' + article.entries[n].link
+      st.write(a)
